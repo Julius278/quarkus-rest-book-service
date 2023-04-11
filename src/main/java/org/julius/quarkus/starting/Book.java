5fb0fd46f314a8afa69bf4ed572b9d728fc40bdd
julius278 <julius.lauterbach@t-online.de>
2023-04-11 15:10:53 +0200

    Getter und Setter ersetzt durch Lombok Annotation

--- src/main/java/org/julius/quarkus/starting/Book.java
+++ src/main/java/org/julius/quarkus/starting/Book.java
@@ -1,8 +1,12 @@
 package org.julius.quarkus.starting;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class Book {
 
     private int id;
@@ -10,46 +14,6 @@ public class Book {
     private String author;
     private String genre;
     private int yearOfPublication;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getYearOfPublication() {
-        return yearOfPublication;
-    }
-
-    public void setYearOfPublication(int yearOfPublication) {
-        this.yearOfPublication = yearOfPublication;
-    }
 
     @Override
     public String toString() {
