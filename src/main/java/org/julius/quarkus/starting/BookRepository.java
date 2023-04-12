e73e53683c639c3bdc9beddfd674ecba5844617a
julius278 <julius.lauterbach@t-online.de>
2023-04-12 12:18:39 +0200

    Konfiguration über application.properties

--- src/main/java/org/julius/quarkus/starting/BookRepository.java
+++ src/main/java/org/julius/quarkus/starting/BookRepository.java
@@ -1,17 +1,22 @@
 package org.julius.quarkus.starting;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.PathParam;
 import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
 public class BookRepository {
+
+    @ConfigProperty(name = "books.genre")
+    private String defaultGenre;
+
     public List<Book> getAllBooks() {
         return List.of(
-                new Book(1, "Java ist auch eine Insel", "Christian Ullenboom", "IT", 2003),
-                new Book(2, "Modern Java in Action", "Raoul-Gabriel Urma, Mario Fusco, Alan Mycroft", "IT", 2017),
-                new Book(3, "Beginning Quarkus Framework", "Tayo Koleoso", "IT", 2020),
+                new Book(1, "Java ist auch eine Insel", "Christian Ullenboom", defaultGenre, 2003),
+                new Book(2, "Modern Java in Action", "Raoul-Gabriel Urma, Mario Fusco, Alan Mycroft", defaultGenre, 2017),
+                new Book(3, "Beginning Quarkus Framework", "Tayo Koleoso", defaultGenre, 2020),
                 new Book(4, "Der Erbe der Jedi-Ritter", "Kevin Hearne", "sci-fi", 2015)
         );
     }
