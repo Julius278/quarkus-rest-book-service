176b4ccddb8bf2b7bab68efd54c362361047175d
julius278 <julius.lauterbach@t-online.de>
2023-04-27 15:34:01 +0200

    fallback tests

--- src/test/java/org/julius/quarkus/starting/BookResourceTest.java
+++ src/test/java/org/julius/quarkus/starting/BookResourceTest.java
@@ -1,6 +1,8 @@
 package org.julius.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.internal.http.Status;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.MediaType;
@@ -18,7 +20,7 @@ public class BookResourceTest {
                 .when()
                 .get("/api/books/world")
                 .then()
-                .statusCode(200)
+                .statusCode(Status.SUCCESS.ordinal())
                 .body("author", is("world"))
                 .body("genre", is("sci-fi"))
                 .body("title", is("hello"))
